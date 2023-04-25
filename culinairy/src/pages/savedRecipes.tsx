03c70f6243806bf02961001629c5afd081702609
JakeSifo <bearcoco5@gmail.com>
2023-04-25 03:52:18 -0400

    Added favicon, added spinner, fixed multi-select overflow, added ads.txt because if my friends keep using this I'll need to pay for chatGPT lol

--- culinairy/src/pages/savedRecipes.tsx
+++ culinairy/src/pages/savedRecipes.tsx
@@ -11,11 +11,18 @@ export default function SavedRecipes() {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [showFavorites, setShowFavorites] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    getUserRecipes(user.uid).then((res) => {
-      setRecipes(res);
-    });
+    getUserRecipes(user.uid)
+      .then((res) => {
+        setRecipes(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }, [user.uid]);
 
   const toggleFavorite = async (recipe: Recipe) => {
@@ -69,38 +76,44 @@ export default function SavedRecipes() {
               {showFavorites ? 'Show All' : 'Show Favorites'}
             </button>
           </div>
-          {filteredRecipes.length > 0 ? (
-            filteredRecipes.map((recipe) => (
-              <div
-                key={recipe.name}
-                id="markdown"
-                className="text-left flex justify-between items-start"
-              >
-                <div>
-                  <button
-                    className={`fa fa-star text-[2em]`}
-                    style={{
-                      color: recipe.favorite ? '#eed21b' : 'rgb(214 211 209 / var(--tw-text-opacity))'
-                    }}
-                    onClick={() => toggleFavorite(recipe)}
-                  />
-                  <Markdown>{recipe.recipe}</Markdown>
-                </div>
-                <button
-                  className={'fa fa-print text-[2em] text-stone-900 mr-5'}
-                  onClick={() => printRecipe(recipe)}
-                />
-                <button
-                  className={'fa-solid fa-trash-can text-[2em] text-stone-900'}
-                  onClick={() => deleteRecipe(recipe)}
-                />
-              </div>
-            ))
-          ) : (
-            <div id="markdown">
-              <Markdown>There are no saved recipes to display.</Markdown>
+          {loading ? (
+            <div className="text-4xl text-gray-500">
+              <i className="fas fa-gear fa-spin text-[2em] text-[#5259ad]"></i>
             </div>
-          )}
+          ) : (
+              filteredRecipes.length > 0 ? (
+                filteredRecipes.map((recipe) => (
+                  <div
+                    key={recipe.name}
+                    id="markdown"
+                    className="text-left flex justify-between items-start"
+                  >
+                    <div>
+                      <button
+                        className={`fa fa-star text-[2em]`}
+                        style={{
+                          color: recipe.favorite ? '#eed21b' : 'rgb(214 211 209 / var(--tw-text-opacity))'
+                        }}
+                        onClick={() => toggleFavorite(recipe)}
+                      />
+                      <Markdown>{recipe.recipe}</Markdown>
+                    </div>
+                    <button
+                      className={'fa fa-print text-[2em] text-stone-900 mr-5'}
+                      onClick={() => printRecipe(recipe)}
+                    />
+                    <button
+                      className={'fa-solid fa-trash-can text-[2em] text-[#C96C45]'}
+                      onClick={() => deleteRecipe(recipe)}
+                    />
+                  </div>
+                ))
+              ) : (
+                <div id="markdown">
+                  <Markdown>There are no saved recipes to display.</Markdown>
+                </div>
+              )
+            )}
         </div>
       )}
       {loggedIn !== undefined && !loggedIn && (
